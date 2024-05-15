b5bd7e2c751e7a990838615a86b990ac3bd72440
KimHyeonBong <vongright@gmail.com>
2024-05-15 23:17:32 +0900

    feature: 요구 기능 구현
    리팩토링 필요
      input 입력을 마치는 조건 보완, 입력을 받는 부분 메서드로 분리,
      input 값이 커스텀 구분자를 가지고 있을 때, 구분자 추출 및 구분자 지정 문구 자르는 로직 보완,
      Scanner, BufferedReader 로 구현 되는지 확인,

--- src/main/java/org/vong/string/calculator/Main.java
+++ src/main/java/org/vong/string/calculator/Main.java
@@ -2,6 +2,7 @@ package org.vong.string.calculator;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -12,35 +13,45 @@ public class Main {
         Tokenizer tokenizer = new Tokenizer();
         Mapper mapper = new Mapper();
 
+        Scanner sc = new Scanner(System.in);
+
         String[] arr;
+        String delimiter;
         long[] numbers;
         long sum;
 
         while (true) {
-            Scanner sc = new Scanner(System.in);
 
             System.out.println("""
+                    ===
                     계산할 값을 구분자와 함께 입력해주세요.
                     ; 과 같은 구분자를 지정하고 싶다면 //; 을 입력 후 엔터를 누르고 계산할 값 지정 구분자와 함꼐 입력해주세요.
+                    ===
                     """);
 
             StringBuilder inputBuilder = new StringBuilder();
+
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
+
                 if (!line.isEmpty()) {
                     inputBuilder.append(line).append("\n");
                 }
-                if (line.isEmpty()) break;
+
+                String lineEnd = line.substring(line.length() - 1);
+
+                if (Pattern.matches("\\d$", lineEnd)) break;
             }
             String input = inputBuilder.toString().trim(); // 마지막 "\n" 제거
 
             System.out.println(input);
 
             try {
+                delimiter = extractor.extractDelimiter(input);
                 if (checker.caseOf(input).equals(PatternChecker.CUSTOM_DELIMITER_REGEX)) {
                     input = input.substring(4);
                 }
-                arr = tokenizer.splitWithDelimiter(extractor.extractDelimiter(input), input);
+                arr = tokenizer.splitWithDelimiter(delimiter, input);
                 numbers = mapper.mapStringToLong(arr);
 
                 break;
