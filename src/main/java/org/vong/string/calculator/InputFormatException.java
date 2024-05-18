6f28d60f279f2f91af5f85e8b9d382e50ce55c38
KimHyeonBong <vongright@gmail.com>
2024-05-18 17:42:23 +0900

    feat: 정규표현식 추가

--- src/main/java/org/vong/string/calculator/InputFormatException.java
+++ src/main/java/org/vong/string/calculator/InputFormatException.java
@@ -1,10 +1,10 @@
 package org.vong.string.calculator;
 
 public class InputFormatException extends RuntimeException {
-    public InputFormatException() {
-        super("""
-                형식에 맞지 않는 입력입니다.
-                입력값은 양의 정수와 구분자로 구성해야합니다.
-                """);
+    public InputFormatException(String input) {
+        super(String.format("""
+                %s: 형식에 맞지 않는 입력입니다.
+                입력값은 정수와 연산자로 구성해야합니다.
+                """, input));
     }
 }
--- src/test/java/org/vong/string/calculator/PatternCheckerTest.java
+++ src/test/java/org/vong/string/calculator/PatternCheckerTest.java
@@ -1,6 +1,7 @@
 package org.vong.string.calculator;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class PatternCheckerTest {
     private PatternChecker checker;
@@ -10,4 +11,11 @@ class PatternCheckerTest {
         checker = new PatternChecker();
     }
 
+    @Test
+    void patternCheck() {
+        for (String input : Fixture.PASS_INPUTS) {
+            System.out.println(input);
+            checker.isComputable((input));
+        }
+    }
 }
